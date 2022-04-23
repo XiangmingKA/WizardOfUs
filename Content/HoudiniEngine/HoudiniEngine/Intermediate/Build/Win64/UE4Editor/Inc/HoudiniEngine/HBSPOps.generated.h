0c6305997cde0bd0048ebd03265086e65c00bf55
XiangmingKa <715524834@qq.com>
2022-04-23 14:09:29 -0400

    Add content

---
+++ Content/HoudiniEngine/HoudiniEngine/Intermediate/Build/Win64/UE4Editor/Inc/HoudiniEngine/HBSPOps.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HOUDINIENGINE_HBSPOps_generated_h
+#error "HBSPOps.generated.h already included, missing '#pragma once' in HBSPOps.h"
+#endif
+#define HOUDINIENGINE_HBSPOps_generated_h
+
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_SPARSE_DATA
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_RPC_WRAPPERS
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_RPC_WRAPPERS_NO_PURE_DECLS
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUHBspPointsGrid(); \
+	friend struct Z_Construct_UClass_UHBspPointsGrid_Statics; \
+public: \
+	DECLARE_CLASS(UHBspPointsGrid, UObject, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/HoudiniEngine"), NO_API) \
+	DECLARE_SERIALIZER(UHBspPointsGrid)
+
+
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_INCLASS \
+private: \
+	static void StaticRegisterNativesUHBspPointsGrid(); \
+	friend struct Z_Construct_UClass_UHBspPointsGrid_Statics; \
+public: \
+	DECLARE_CLASS(UHBspPointsGrid, UObject, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/HoudiniEngine"), NO_API) \
+	DECLARE_SERIALIZER(UHBspPointsGrid)
+
+
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UHBspPointsGrid(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UHBspPointsGrid) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UHBspPointsGrid); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UHBspPointsGrid); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UHBspPointsGrid(UHBspPointsGrid&&); \
+	NO_API UHBspPointsGrid(const UHBspPointsGrid&); \
+public:
+
+
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UHBspPointsGrid(UHBspPointsGrid&&); \
+	NO_API UHBspPointsGrid(const UHBspPointsGrid&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UHBspPointsGrid); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UHBspPointsGrid); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UHBspPointsGrid)
+
+
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_PRIVATE_PROPERTY_OFFSET
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_157_PROLOG
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_PRIVATE_PROPERTY_OFFSET \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_SPARSE_DATA \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_RPC_WRAPPERS \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_INCLASS \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_PRIVATE_PROPERTY_OFFSET \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_SPARSE_DATA \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_RPC_WRAPPERS_NO_PURE_DECLS \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_INCLASS_NO_PURE_DECLS \
+	Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h_160_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> HOUDINIENGINE_API UClass* StaticClass<class UHBspPointsGrid>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Engine_Plugins_Runtime_HoudiniEngine_Source_HoudiniEngine_Private_HBSPOps_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
